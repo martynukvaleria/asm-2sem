9307e1a1e1cf5118a5dc6513932e885a730def0e
Martynuk Valeria <martynukvalery2004@gmail.com>
2022-03-04 17:48:28 +0300

    added second part of lab2

---
+++ lab2/lab2_second.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main(){
+	int number, sum(0);
+	std :: cout << "Enter natural number : ";
+	std::cin >> number;
+	__asm {
+		mov ecx, sum
+		cmp number, 0
+		jl end_of_program
+
+		start :	
+
+		mov ebx, 10
+		mov edx, 0
+		mov eax, number
+		div ebx
+
+		imul ecx, ebx
+		add ecx, edx
+		mov number, eax
+		cmp number, 0
+		jne start
+
+
+		end_of_program :
+		mov sum, ecx
+	}
+	std::cout << "Inverted number : " << sum ;
+}
